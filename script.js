5ce534bd4f58a6c68628c81f0aace60ba2147d42
Roman <bolist@mail.ru>
2018-06-17 21:52:48 +0300

    2 lesson made menu

---
+++ script.js
@@ -0,0 +1,10 @@
+let menu = document.getElementById('menu');
+menu.onclick = function myFunction() {
+        let x = document.getElementById('myTopnav');
+    if (x.className === "topnav") {
+        x.className += "  responsive";
+    } else {
+        x.className = "topnav";
+    }
+};
+
